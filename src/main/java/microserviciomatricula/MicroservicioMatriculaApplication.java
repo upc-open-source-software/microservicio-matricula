3870f6f1f130e474d80429dea0a130b1544e8ad3
Luis Guisado <lguisadom@gmail.com>
2023-01-30 16:56:07 -0500

    First commit

---
+++ src/main/java/microserviciomatricula/MicroservicioMatriculaApplication.java
@@ -0,0 +1,13 @@
+package microserviciomatricula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioMatriculaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioMatriculaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/microserviciomatricula/MicroservicioMatriculaApplicationTests.java
@@ -0,0 +1,13 @@
+package microserviciomatricula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioMatriculaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
